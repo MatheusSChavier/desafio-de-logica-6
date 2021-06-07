92fb5cb3e08653d634900621910978f976d56419
kenykenoei <matheuschavier112004@gmail.com>
2021-06-07 01:41:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let I={
+    name:"Matheus",
+    age:17,
+    favoritFood:"Pizza",
+}
+I.favoritGame="Dark Souls"
+delete I.favoritFood
+console.log(I)
+let cadrastro=[
+    {
+        nome:"Fulano",
+        idade:1,
+        telefone:08001234,
+        amigos:["Ciclano","Faustão","Santos","Luciano"]
+    },
+    {
+        nome:"Ciclano",
+        idade:2,
+        telefone:80881234,
+        amigos:["Fulano","Faustão","Santos","Luciano"]
+    },
+    {
+        nome:"Faustão",
+        idade:3,
+        telefone:08004321,
+        amigos:["Santos","Ciclano","Fulano","Luciano"],
+    },
+    {
+        nome:"Santos",
+        idade:4,
+        telefone:80884321,
+        amigos:["Luciano","Ciclano","Fulano","Faustão"],
+    },
+    {
+        nome:"Luciano",
+        idade:5,
+        telefone:98761234,
+        amigos:["Faustão","Ciclano","Fulano","Santos"],
+    }
+]
+console.log(cadrastro)
